71de040420ca3205aa86324b255e64c67c6e8d80
nelifs <paaalmachka@gmail.com>
2023-12-13 22:36:22 +0300

    added numeric code to gateway enum and added api endpoints enum

--- foxogram-common/src/main/java/su/foxogram/enums/API.java
+++ foxogram-common/src/main/java/su/foxogram/enums/API.java
@@ -2,10 +2,36 @@ package su.foxogram.enums;
 
 public class API {
     public enum Routes {
+        DATA("/data"),
+        AUTH("/auth");
 
+        private final String route;
+
+        Routes(final String route) {
+            this.route = route;
+        }
+
+        @Override
+        public String toString() {
+            return route;
+        }
     }
 
     public enum Endpoints {
+        MESSAGES(Routes.DATA.route + "/messages"),
+        USERS(Routes.DATA.route + "/users"),
+        ATTACHMENTS(Routes.DATA.route + "/attachments"),
+        AVATARS(Routes.DATA.route + "/avatars");
 
+        private final String endpoint;
+
+        Endpoints(final String endpoint) {
+            this.endpoint = endpoint;
+        }
+
+        @Override
+        public String toString() {
+            return endpoint;
+        }
     }
 }
--- foxogram-common/src/main/java/su/foxogram/enums/Gateway.java
+++ foxogram-common/src/main/java/su/foxogram/enums/Gateway.java
@@ -2,12 +2,23 @@ package su.foxogram.enums;
 
 public class Gateway {
     public enum Events {
-        MESSAGE_CREATE,
-        MESSAGE_UPDATE,
-        MESSAGE_DELETE,
-        USER_UPDATE,
-        HELLO,
-        HEARTBEAT,
-        READY;
+        MESSAGE_CREATE("01"),
+        MESSAGE_UPDATE("02"),
+        MESSAGE_DELETE("03"),
+        USER_UPDATE("04"),
+        HELLO("05"),
+        HEARTBEAT("06"),
+        READY("07");
+
+        private final String event;
+
+        Events(final String event) {
+            this.event = event;
+        }
+
+        @Override
+        public String toString() {
+            return event;
+        }
     }
 }
