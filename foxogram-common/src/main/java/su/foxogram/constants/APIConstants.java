1d7a48c6678d7826b54507c1ee0a70bbfbb832ff
daniel <danil.ch9@proton.me>
2024-12-06 20:53:23 +0300

    Remove /v1 prefix

--- foxogram-common/src/main/java/su/foxogram/constants/APIConstants.java
+++ foxogram-common/src/main/java/su/foxogram/constants/APIConstants.java
@@ -1,11 +1,11 @@
 package su.foxogram.constants;
 
 public class APIConstants {
-	public static final String MESSAGES = "/v1/messages";
+	public static final String MESSAGES = "/messages";
 
-	public static final String USERS = "/v1/users";
+	public static final String USERS = "/users";
 
-	public static final String AUTH = "/v1/auth";
+	public static final String AUTH = "/auth";
 
-	public static final String CHANNELS = "/v1/channels";
+	public static final String CHANNELS = "/channels";
 }
