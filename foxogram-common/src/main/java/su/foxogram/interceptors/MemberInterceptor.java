519a843e0806f05fc9b21cd3182f003938cd0fba
nelifs <nelifs@icloud.com>
2024-12-13 17:32:01 +0300

    Add check if member is null

--- foxogram-common/src/main/java/su/foxogram/interceptors/MemberInterceptor.java
+++ foxogram-common/src/main/java/su/foxogram/interceptors/MemberInterceptor.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import su.foxogram.constants.AttributesConstants;
+import su.foxogram.exceptions.ChannelNotFoundException;
 import su.foxogram.models.Channel;
 import su.foxogram.models.Member;
 import su.foxogram.models.User;
@@ -23,11 +24,13 @@ public class MemberInterceptor implements HandlerInterceptor {
 	}
 
 	@Override
-	public boolean preHandle(HttpServletRequest request, @NotNull HttpServletResponse response, @NotNull Object handler) {
+	public boolean preHandle(HttpServletRequest request, @NotNull HttpServletResponse response, @NotNull Object handler) throws ChannelNotFoundException {
 		User user = (User) request.getAttribute(AttributesConstants.USER);
 		Channel channel = (Channel) request.getAttribute(AttributesConstants.CHANNEL);
 
 		Member member = channelsService.getMember(channel, user.getUsername());
+
+		if (member == null) throw new ChannelNotFoundException();
 
 		request.setAttribute(AttributesConstants.MEMBER, member);
 		return true;
