eb43161aeefa3670645e77d487730e9234fab5d1
nelifs <nelifs@icloud.com>
2024-12-16 00:12:10 +0300

    Fix bucket name

--- foxogram-common/src/main/java/su/foxogram/constants/BucketsConstants.java
+++ foxogram-common/src/main/java/su/foxogram/constants/BucketsConstants.java
@@ -3,5 +3,5 @@ package su.foxogram.constants;
 public class BucketsConstants {
 	public static final String AVATARS_BUCKET = "avatars";
 
-	public static final String ATTACHMENTS_BUCKET = "avatars";
+	public static final String ATTACHMENTS_BUCKET = "attachments";
 }
