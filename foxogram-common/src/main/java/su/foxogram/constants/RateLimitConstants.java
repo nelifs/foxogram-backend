90765f4b0bd476fee6aa7d2e07ea786e6153b048
daniel <danil.ch9@proton.me>
2024-12-17 22:00:27 +0300

    Increase rate limits

--- foxogram-common/src/main/java/su/foxogram/constants/RateLimitConstants.java
+++ foxogram-common/src/main/java/su/foxogram/constants/RateLimitConstants.java
@@ -3,9 +3,9 @@ package su.foxogram.constants;
 public class RateLimitConstants {
 	public static final int RATE_LIMIT_CONSUME = 1;
 
-	public static final int RATE_LIMIT_CAPACITY = 100;
+	public static final int RATE_LIMIT_CAPACITY = 10;
 
-	public static final int RATE_LIMIT_REFILL = 100;
+	public static final int RATE_LIMIT_REFILL = 10;
 
 	public static final int RATE_LIMIT_DURATION = 1;
 }
